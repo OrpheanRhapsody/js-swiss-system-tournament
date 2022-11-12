3841483bd790219175e801fb96905fc65eb051a3
romain <romain.daveu@gmail.com>
2022-11-12 12:43:45 +0100

    reindentations

--- app/js/main.js
+++ app/js/main.js
@@ -11,8 +11,8 @@ function swissSystem(chessPlayers) {
     
     // /!\ The following only works if the length of chessPlayers is even!
     for (let player = 0; player < (chessPlayers.length)/2; player++) {
-        const s1Player = chessPlayers[player];
-        const s2Player = chessPlayers[player+(chessPlayers.length)/2];
+        const s1Player = chessPlayers[player]
+        const s2Player = chessPlayers[player+(chessPlayers.length)/2]
         matches['match' + (player+1)] = [s1Player, s2Player]
     }
     
@@ -27,15 +27,15 @@ fs.readFile(jsonFile, (err, players) => {
     // Data recovery
     let playersStr = players.toString()
     let playersObj = JSON.parse(playersStr)
-    let matches = swissSystem(playersObj.players);
-    console.log("The first round of matches is:\n");
-    console.log(matches);
-
-    console.log('\n Writing the matches object as app/db/first_round.json ...\n');
+    let matches = swissSystem(playersObj.players)
+    console.log("The first round of matches is:\n")
+    console.log(matches)
+    
+    console.log('\n Writing the matches object as app/db/first_round.json ...\n')
     fs.writeFileSync('app/db/first_round.json', JSON.stringify(matches, null, '\t'))
-
+    
     fs.readFile('app/db/first_round.json', (err, data) => {
-        if (err) throw err;
-        console.log("Successfully written.");
-    });
+        if (err) throw err
+        console.log("Successfully written.")
+    })
 })
